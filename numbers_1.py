3609514334ef78ff05cde3b930a43a0373c282ba
ShadabAfzal7 <shadabafzal00@gmail.com>
2026-08-05 16:41:47 +0530

    Version 2 with square_function

--- numbers_1.py
+++ numbers_1.py
@@ -3,6 +3,9 @@ def even(a):
    return True
  else:
   return False
+def square(a):
+  return (a*a)
 if __name__ == "__main__":
   x = int(input("Enter the number"))
   print("Is it Even number : ", even(x))
+  print("The square of X is : ",square(x))
