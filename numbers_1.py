5b9b0eb5c83a71fc7a6e721d14957f3d1873cccb
ShadabAfzal7 <shadabafzal00@gmail.com>
2026-08-05 16:50:57 +0530

    Version 3 with cube__function

--- numbers_1.py
+++ numbers_1.py
@@ -5,7 +5,10 @@ def even(a):
   return False
 def square(a):
   return (a*a)
+def cube(a):
+  return (a*a*a)
 if __name__ == "__main__":
   x = int(input("Enter the number"))
   print("Is it Even number : ", even(x))
   print("The square of X is : ",square(x))
+  print("The cube of X is : ",cube(x))
